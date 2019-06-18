9b6dd51bc1c7529e167d8cce920e889a53949a81
notozeki <notozeki@gmail.com>
2019-06-18 16:00:54 +0900

    Backend: Enable CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET',
+    allowedHeaders: 'Content-Type',
+  });
   await app.listen(3000);
 }
 bootstrap();
