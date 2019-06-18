4df5da000f8d002ce6a6d40bf7cc1c43072891e5
notozeki <notozeki@gmail.com>
2019-06-18 13:12:28 +0900

    Backend: Add test for `GET /books`

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -1,5 +1,8 @@
+import * as path from 'path';
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
+import * as SwaggerParser from 'swagger-parser';
+import { Validator } from 'jsonschema';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
@@ -19,5 +22,22 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  it('/books (GET)', async () => {
+    const parser = new SwaggerParser();
+    const spec = await parser.dereference(path.resolve(__dirname, '../../openapi.yaml'));
+    const schema = spec.paths['/books'].get.responses['200'].content['application/json'].schema;
+    const validator = new Validator();
+
+    return request(app.getHttpServer())
+      .get('/books')
+      .expect(200)
+      .expect(({ body }) => {
+        const result = validator.validate(body, schema);
+        if (!result.valid) {
+          throw new Error(result.errors.toString());
+        }
+      });
   });
 });
